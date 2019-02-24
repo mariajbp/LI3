a8ace984096c812753fdc42fe3730fe680acf929
mariajbp <mariajbp00@gmail.com>
2019-02-24 20:08:05 +0000

    Estrutura das vendas e funções sobre ela

---
+++ include/vendas.h
@@ -0,0 +1,85 @@
+
+#ifndef __VENDAS_H__
+#define __VENDAS_H__
+
+/**
+ * o que faz 
+ * @param  
+ * @returns uma Venda
+ */
+
+Venda createVenda()
+
+
+/**
+ * Retorna o código do cliente
+ * @param 
+ * @returns o código do cliente
+ */
+char* getPCode(Venda v);
+
+/**
+ * Retorna o preço de um produto
+ * @param  
+ * @returns o preço de um produto
+ */
+double price(Venda v)
+
+/**
+ * o que faz
+ * @param  
+ * @returns o nº de unidades vendidas
+ */
+int units(Venda v)
+
+
+/**
+ * o que faz
+ * @param  
+ * @returns o tipo de venda (Promoção ou Normal)
+ */
+char np(Venda v)
+
+
+ /**
+ * o que faz
+ * @param  
+ * @returns o código do cliente
+ */
+char* clcode(Venda v)
+
+
+/**
+ * o que faz
+ * @param  
+ * @returns o mês da venda
+ */
+int month(Venda v)
+
+/**
+ * o que faz
+ * @param  
+ * @returns o nº da filial onde foi efetuada a comprada
+ */
+int filial(Venda v)
+
+
+
+/**
+ * o que faz
+ * @param  
+ * @returns 
+ */
+
+
+/**
+ * o que faz
+ * @param  
+ * @returns 
+ */
+
+
+
+
+
+#endif
