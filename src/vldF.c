673ea6583b6ff7e8bda1a4b6a4363c07162ce81a
Francisco Peixoto <thtr.irubra@gmail.com>
2019-03-05 13:01:02 +0000

    ta tudo pronto

--- src/vldF.c
+++ src/vldF.c
@@ -7,25 +7,25 @@
 int validaProduto(char * id, int i){
 	int r = 0;
 		if(!id[i])														//segue se o id[i] é \0 ou NULL
-			if(isupper(id[0]) && isupper(id[1]))										//Se os dois primeiros carateres sao maiusculas 								
-				if(('1'<=id[2]) && (id[2]<='9'))									//Se a 3ª posição é entre 1 e 9
+			if(isupper(id[0]) && isupper(id[1]))						//Se os dois primeiros carateres sao maiusculas 								
+				if(('1'<=id[2]) && (id[2]<='9'))						//Se a 3ª posição é entre 1 e 9
 					for(int n = 3; n < (i-1); n++){
 						if(isdigit(id[n])) r = 1;
 						else return 0;
-					}			
+					}
 	return r;
 }
 
 //função que valida um id de um cliente
 int validaCliente(char * id, int i){
 	int r = 0;
-		if(!id[i])											//segue se o id[i] é \0 ou NULL
-			if(isupper(id[0]))									//Se a primeiros carateres sao maiusculas 		
+		if(!id[i])														//segue se o id[i] é \0 ou NULL
+			if(isupper(id[0]))											//Se a primeiros carateres sao maiusculas 		
 				if(('1'<=id[1]) && (id[1]<='5'))						//Se a 2ª posição é entre 1 e 5
 					for(int n = 2; n < (i-1); n++){
 						if(isdigit(id[n])) r = 1;
 						else return 0;
-					}			
+					}
 	return r;
 }
 
@@ -41,27 +41,29 @@ int searchID(const char * lista[], const char * id){
 }
 
 //função que valida um id de uma venda
-int validaVendas(char* linha, int i, const char* produtos[], const char* clientes[]){
-	int r = 0; // s[10];
+int validaVendas(char* linha, const char* produtos[], const char* clientes[]){
+	int r = 0, id = 0;
 	char* tok = NULL;
+	tok = strtok(linha, " ");
+	char* tokens[6];
 
-	if(i<=31){													//Se i for > 31, descarta logo
-		tok = strtok(linha, " ");
-		if( searchID(produtos , tok) ){
-			tok = strtok(NULL, " ");
-			if( atof(tok) <= 999.99 && atof(tok) >= 0.0 ){		// atof(str) converte a str para float, pertence a string.h
-				tok = strtok(NULL, " ");
-				if( atoi(tok) <= 200 && atoi(tok) >= 1 ){		// atoi(str) converte a str para int, pertence a string.h
-					tok = strtok(NULL, " ");
-					if( strcmp(tok, "N") || strcmp(tok, "P") ){
-						tok = strtok(NULL, " ");
-						if( searchID(clientes , tok) ){
-							tok = strtok(NULL, " ");
-							if( atoi(tok) <= 12 && atoi(tok) >= 1 ){
-								tok = strtok(NULL, " ");
-								if( atoi(tok) <= 3 && atoi(tok) >= 1 ) // validar a filial
-											return 1;}}}}}}
-	}
+    while(tok) {															// verificar a quantidade de sub strings na linha
+    	if(id < 7){
+    		tokens[id] = strdup(tok);
+    	}
+        tok = strtok(NULL," ");
+        id++;
+    } 
+
+	if(id == 7)																// se tokens tiver 7 posicoes, estas devem ser testadas
+		if( searchID(produtos , tokens[0]) )
+			if( atof(tokens[1]) <= 999.99 && atof(tokens[1]) >= 0.0 )		// atof(str) converte a str para float, pertence a string.h
+				if( atoi(tokens[2]) <= 200 && atoi(tokens[2]) >= 1 )		// atoi(str) converte a str para int, pertence a string.h
+					if( strcmp(tokens[3], "N") || strcmp(tokens[3], "P") )
+						if( searchID(clientes , tokens[4]) )
+							if( atoi(tokens[5]) <= 12 && atoi(tokens[5]) >= 1 )
+								if( atoi(tokens[6]) <= 3 && atoi(tokens[6]) >= 1 ) // validar a filial
+											r = 1;
 	return r;
 }
 
@@ -70,12 +72,14 @@ int main(){
 	const char *pp[] = {"KR1583", "DV1294", "FF2916"};
 	const char *cc[] = {"L4891"};
 	char c[] = "KR1583 77.72 128 P L4891 2 1";
+	char c2[] = "KR1583 77.72 128 P L4892 2 1";
 
 	printf("\nVALIDA CLIENTE/PRODUTO\n\n");
 	printf("valida A deve dar 1 e 0, da - %d %d\n", validaCliente(a,5), validaProduto(a,6) );
 	printf("valida B deve dar 0 e 1, da - %d %d\n", validaCliente(b,5), validaProduto(b,6) );
 	
 	printf("\nVALIDA VENDAS\n\n");
-	printf("\ndeve dar 1 = %d\n", validaVendas(c,31, pp, cc ) );
+	printf("\ndeve dar 1 = %d\n", validaVendas(c, pp, cc ) );
+	printf("\ndeve dar 0 = %d\n", validaVendas(c2, pp, cc ) );
 	return 0;
 }
