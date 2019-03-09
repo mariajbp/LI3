e673d26910168a029dcd7e25b4e7d52774b1395b
Francisco Peixoto <thtr.irubra@gmail.com>
2019-03-09 19:23:21 +0000

    1

---
+++ src/load.h
@@ -0,0 +1,7 @@
+#ifndef __LOAD_H__
+#define __LOAD_H__
+
+
+char** loadArray( char** array, char* path, const char id, int max, const char* produtos[], const char* clientes[]);
+
+#endif
---
+++ src/valida.h
@@ -0,0 +1,17 @@
+#ifndef __VALIDA_H__
+#define __VALIDA_H__
+
+//função que valida um id de um produto
+int validaProduto(char * id, int i);
+
+//função que valida um id de um cliente
+int validaCliente(char * id, int i);
+
+// função que valida se uma string está no array de strings
+int searchID(const char * lista[], const char * id);
+
+//função que valida um id de uma venda
+int validaVenda(char* linha, const char* produtos[], const char* clientes[]);
+
+
+#endif
