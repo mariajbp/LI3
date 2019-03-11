dae12b2ceeb8759d8cbed104bb4e824bda638c22
mimr21 <mariamiguel211999@gmail.com>
2019-03-11 16:24:18 +0000

    Updated load.h

--- src/load.h
+++ src/load.h
@@ -4,4 +4,9 @@
 
 char** loadArray( char** array, char* path, const char id, int max, const char* produtos[], const char* clientes[]);
 
+
+char** lArray( char** array, char* path, int max, int (*valida) (char, ...) );
+
+
+int linecount(char* path);
 #endif
