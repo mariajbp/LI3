0782ca14c2cd87178c1742a7256aefd45f84a46a
mimr21 <mariamiguel211999@gmail.com>
2019-03-12 15:33:46 +0000

    Load.c update, makefile done

--- src/load.c
+++ src/load.c
@@ -3,47 +3,11 @@
 #include <string.h>
 #include "valida.h"
 
-// função que lê um ficheiro, valida as linhas e carrega essas linhas num array 
-char** loadArray( char** array, char* path, const char id, int max, const char* produtos[], const char* clientes[]){
-	char * linha = NULL;
-	int i = 0;
-	FILE* file;
-	switch(id){
-		case 'c':
-			file = fopen(path , "r");
-			while( fgets(linha, max, file) ){
-				if(validaCliente(linha, 5)){
-					array[i] = strdup(linha);
-					i++;
-				}
-			}
-			fclose(file);
-			break;
-		case 'p':
-			file = fopen(path , "r");
-			while( fgets(linha, max, file) ){
-				if(validaProduto(linha, 6)){
-					array[i] = strdup(linha);
-					i++;
-				}
-			}
-			fclose(file);
-			break;
-		case 'v':
-			file = fopen(path , "r");
-			while( fgets(linha, max, file) ){
-				if(validaVenda(linha, produtos, clientes)){
-					array[i] = strdup(linha);
-					i++;
-				}
-			}
-			fclose(file);
-			break;
-	}
-	return array;
-}
+#define _GNU_SOURCE
+#define _SVID_SOURCE //-->nao dar warning na compilaçao
 
-char** lArray( char** array, char* path, int max, int (*valida) (char, ...) ){
+char** lArray( char* path, int max, int (*valida) (char, ...) ){
+	char** array = 
 	char * linha = NULL;
 	int i = 0;
 	FILE* file;
