dda1cf77b8a2fe6219778936a244713fbd1a462d
Francisco Peixoto <thtr.irubra@gmail.com>
2019-03-13 00:01:59 +0000

    quality of life changes, no more warnings

--- src/load.c
+++ src/load.c
@@ -3,56 +3,53 @@
 #include "load.h"
 
 
-
 //Faz load de um ficheiro no array
-/*char** lArray( char* path, int max, int (*valida) (char, ...) ){
-	char** array = 
-	char * linha = NULL;
+char** loadArray( char** array, char* path, int max, int (*valida) (char, ...) ){
+	char * linha = " ";
 	int i = 0;
-	FILE* file;
+	FILE* file = fopen(path , "r");
+	
+	if(file == NULL){
+      		printf("Error! You tried to read an empty file.");   
+     		exit(1);             
+    	}
 
-	file = fopen(path , "r");
-	if(file == NULL)
-    {
-      printf("Error!");   
-      exit(1);             
-    }
 	while( fgets(linha, max, file) ){
 		if(valida){
 			array[i] = strdup(linha);
 			i++;
 		}
 	}
+	
 	fclose(file);	
 
 	return array;
-}*/
+}
 
 //max = função de profilling - fazer fora 
-
 int proffiling()
 {
-	
+	return 0;	
 }
 
 
 //ADICIONAR AO FILE DAS QUERIES MAYBE
-int linecount(char* path)
-{
-	int lines, ch;
+int linecount(char* path){
+	int lines = 0, ch;
 	FILE* fp = fopen(path, "r");
-	if(fp == NULL)
-    {
-      printf("Error!");   
-      exit(1);             
-    }
-	while(!feof(fp))
-	{
+	
+	if(fp == NULL){
+      		printf("Error!");   
+      		exit(1);             
+    	}
+	
+	while(!feof(fp)){
   		ch = fgetc(fp);
   		if(ch == '\n') lines++;
 	}
-
+	
 	fclose(fp);
+	
 	return lines;
 }
 
