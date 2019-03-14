7651b6e4a951fe8acf0bc258b8fbca4b8544effb
mimr21 <mariamiguel211999@gmail.com>
2019-03-14 10:05:58 +0000

    We have a decent main now, wrFile done, makefile updated

---
+++ src/exe.c
@@ -0,0 +1,23 @@
+#include "valida.h"
+#include "load.h"
+#include "vendas.h"
+
+//Main do projeto
+
+
+int main(){
+	char a[] = "F2916", b[] = "DV1294";
+	const char *pp[] = {"KR1583", "DV1294", "FF2916"};
+	const char *cc[] = {"L4891"};
+	char c[] = "KR1583 77.72 128 P L4891 2 1";
+	char c2[] = "KR1583 77.72 128 P L4892 2 ";
+
+	printf("\nVALIDA CLIENTE/PRODUTO\n\n");
+	printf("valida A deve dar 1 e 0, da - %d %d\n", validaCliente(a,5), validaProduto(a,6) );
+	printf("valida B deve dar 0 e 1, da - %d %d\n", validaCliente(b,5), validaProduto(b,6) );
+	
+	printf("\nVALIDA VENDAS\n\n");
+	printf("\ndeve dar 1 = %d\n", validaVenda(c , pp, cc ) );
+	printf("\ndeve dar 0 = %d\n", validaVenda(c2, pp, cc ) );
+	return 0;
+}
---
+++ src/file.c
@@ -0,0 +1,100 @@
+#define _GNU_SOURCE
+#include "valida.h"
+#include "load.h"
+
+
+
+//Faz load de um ficheiro no array RETORNA QUANTO ESCREVEU NO ARRAY (para a função wrfile)
+int loadArray( char** array, char* path, int max, int (*valida) (char, ...) ){
+	char * linha = " ";
+	int i = 0;
+	FILE* file = fopen(path , "r");
+	
+	if(file == NULL){
+      		printf("Error! You tried to read an empty file.");   
+     		exit(1);             
+    	}
+
+	while( fgets(linha, max, file) ){
+		if(valida){
+			array[i] = strdup(linha);
+			i++;
+		}
+	}
+	
+	fclose(file);	
+
+	return i;
+}
+
+
+//ADICIONAR AO FILE DAS QUERIES MAYBE
+int linecount(char* path){
+	int lines = 0, ch;
+	FILE* fp = fopen(path, "r");
+	
+	if(fp == NULL){
+      		printf("Error!");   
+      		exit(1);             
+    	}
+	
+	while(!feof(fp)){
+  		ch = fgetc(fp);
+  		if(ch == '\n') lines++;
+	}
+	
+	fclose(fp);
+	
+	return lines;
+}
+
+//Função que dado o array com os dados válidos e o tamanho do array, os escreve no ficheiro
+
+int wrFileP (char** array, int n){
+	int r = 0;
+	int i = 0;
+	FILE* fp = fopen("ProdutosValidos.txt", "w+");
+	if(fp == NULL){
+		printf("Error!");
+		r = 1;
+	}
+	while(i>0 && n > 0){
+		i = fprintf(fp,"%s\n", array[i]);
+		n--;
+	}
+	return r;
+
+}
+
+int wrFileC (char** array, int n){
+	int r = 0;
+	int i = 0;
+	FILE* fp = fopen("ClientesValidos.txt", "w+");
+	if(fp == NULL){
+		printf("Error!");
+		r = 1;
+	}
+	while(i>0 && n > 0){
+		i = fprintf(fp,"%s\n", array[i]);
+		n--;
+	}
+	return r;
+
+}
+
+int wrFileV (char** array, int n){
+	int r = 0;
+	int i = 0;
+	FILE* fp = fopen("VendasValidas.txt", "w+");
+	if(fp == NULL){
+		printf("Error!");
+		r = 1;
+	}
+	while(i>0 && n > 0){
+		i = fprintf(fp,"%s\n", array[i]);
+		n--;
+	}
+	return r;
+
+}
+
