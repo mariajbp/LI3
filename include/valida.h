781037669eb0e0505f9f8872a9022b345e9df8e9
Francisco Peixoto <thtr.irubra@gmail.com>
2019-03-23 14:27:35 +0000

    ele nao le o include do hash.h, help

--- include/valida.h
+++ include/valida.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include "hash.h"
 
 /**
  * Função que valida um id de um produto
@@ -21,18 +22,11 @@ int validaProduto(char * id, int i);
 int validaCliente(char * id, int i);
 
 /**
- * Função que valida se uma string está no array de strings
- * @param O QUE RECEBE
- * @param O QUE RETORNA
- */
-int searchID(const char * lista[], const char * id);
-
-/**
  * Função que valida um id de uma venda
  * @param O QUE RECEBE
  * @param O QUE RETORNA
  */
-int validaVenda(char* linha, const char* produtos[], const char* clientes[]);
+int validaVenda(char* linha, hash*** produtos, hash** clientes);
 
 /**
  * Função que dado uma linha e um array, preenche o array com tokens da linha e devolve o número de campos
