78489529c977221d4835628d1d6821caf727e5b5
mariajbp <mariajbp00@gmail.com>
2019-03-25 19:15:16 +0000

    all I did this weekend yay i wanna die

--- src/CatProd.c
+++ src/CatProd.c
@@ -3,10 +3,26 @@
 
 struct catprod
 {
-	
+	struct CAT_PROD* l;
+ 	struct CAT_PROD* r;
+ 	PRODUTOS* p; //ELEMENTO A RECEBER
 };
+	
 
-//CAT_PROD creat_catprod()
+
+CAT_PROD create_catprod()
+{
+	FILE* file = fopen("ProdutosValidos.txt" , "r");
+	if(file == NULL){
+      		printf("Error! You tried to read an empty file.");   
+     		exit(1);             
+    	}
+    //ADICIONAR LINHA A LINHA NUMA ARVORE
+	
+	fclose(file);	
+
+}
+
 
 //GETS 
 
--- src/catcl.c
+++ src/catcl.c
@@ -3,10 +3,29 @@
 
 struct catcl
 {
-	
+	struct CAT_CL* l;
+ 	struct CAT_CL* r;
+ 	CLIENTE* c; //ELEMENTO A RECEBER
 };
 
-//CAT_CL creat_catcl()
+CAT_CL create_catcl()
+{
+	FILE* file = fopen("ClientesValidos.txt" , "r");
+	if(file == NULL){
+      		printf("Error! You tried to read an empty file.");   
+     		exit(1);             
+    	}
+
+	//ADICIONAR LINHA A LINHA NA ARVORE
+	fclose(file);	
+
+}
+
+ void insert(CAT_CL ccl, CLIENTE c)
+ {
+ 	CAT_CL 
+ }
+
 
 //GETS 
 
