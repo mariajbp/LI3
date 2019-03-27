beecc8b04756a1b809fb4a535e3e81b892e95620
Francisco Peixoto <thtr.irubra@gmail.com>
2019-03-27 23:08:09 +0000

    hash.h acabado

--- include/hash.h
+++ include/hash.h
@@ -48,17 +48,17 @@ int search_P(char id[], hash** table[]);
 
 /**
  * Função que imprime a arvore correspondente numa posição da tabela
- * @param file pointer, primeira letra, segunda letra, index da tabela, a arvore e um numero que vai retornar
+ * @param file pointer, primeira letra, segunda letra, a arvore e um numero que vai retornar
  * @param o numero de elementos impressos no ficheiro
  */
-int fprint_hC(FILE* fp, int l1, int h, hash** table, int num);
+int fprint_hC(FILE* fp, int l1, hash table, int num);
 
 /**
  * Função que imprime a arvore correspondente numa posição da tabela
- * @param file pointer, primeira letra, segunda letra, index da tabela, a arvore e um numero que vai retornar
+ * @param file pointer, primeira letra, segunda letra, a arvore e um numero que vai retornar
  * @param o numero de elementos impressos no ficheiro
  */
- int fprint_hP(FILE* fp, int l1, int l2, int h, hash*** table, int num);
+ int fprint_hP(FILE* fp, int l1, int l2, hash table, int num);
 
 
 #endif
