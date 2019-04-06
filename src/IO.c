64ab1e9f9a263ba99c1ccc98859faad1a51c52a5
mariajbp <mariajbp00@gmail.com>
2019-04-07 00:15:25 +0100

    more colorful than my life

--- src/IO.c
+++ src/IO.c
@@ -1,7 +1,7 @@
 #include "../include/IO.h"
-#include <time.h>
+ #include <time.h>
 
-#define KNRM  "\x1B[0m"
+#define RESET  "\x1B[0m"
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
 #define KYEL  "\x1B[33m"
@@ -10,21 +10,22 @@
 #define KCYN  "\x1B[36m"
 #define KWHT  "\x1B[37m"
 
-void print_menu(){
-	printf("\n************************************************** Sistema de Gestão de Vendas ************************************************** \n", KCYN);
-	printf("\n\t-> Query 1:  Ler ficheiros (os ficheiros disponiveis para leitura são: \'Produtos.txt\', \'Clientes.txt\', \'Vendas_1M.txt\').\n");
-	printf("\n\t-> Query 2:  Determinar a lista de produtos começados por uma Letra à escolha (A...Z).\n");
-	printf("\n\t-> Query 3:  Dado um mês e um produto, determinar e apresentar o nº total de registos\n\t   de venda e o total facturado com esse produto nesse mês, tendo em conta a distinção N/P,\n\t   há possibilidade de escolher a apresentação dos resultados dividida por filial.\n");
-	printf("\n\t-> Query 4:  Determinar a lista ordenada de produtos que ninguém comprou, e o seu total,\n\t   há possibilidade de escolher a apresentação dos resultados dividida por filial.\n");
-	printf("\n\t-> Query 5:  Determinar a lista ordenada de clientes que realizaram compras em todas as filiais.\n");
-	printf("\n\t-> Query 6:  Determinar o nº de clientes registados que não realizaram compras.\n\t   Determinar o nº de produtos que ninguém comprou.\n");
-	printf("\n\t-> Query 7:  Dado um cliente, criar uma tabela com o nº total de produtos comprados, mês a mês, organizado por filial.\n");
-	printf("\n\t-> Query 8:  Dado um intervalo de tempo (de mês __ a mês __):\n\t   Determinar o total de vendas registadas nesse intervalo e o total facturado.\n");
-	printf("\n\t-> Query 9:  Dado um produto e uma filial, determinar os clientes e o nº de clientes que o compraram,\n\t   distinguindo a compra entre N e P.\n");
-	printf("\n\t-> Query 10: Dado um cliente e um mês, determinar a lista de\n\t   produtos que esse cliente mais comprou, por ordem decrescente.\n");
-	printf("\n\t-> Query 11: Criar uma lista dos N produtos mais vendidos em todo o ano,\n\t   indicando o nº total de clientes e o nº total de unidades vendidas, filial a filial.\n");
-	printf("\n\t-> Query12: Dado um cliente, determinar os 3 produtos em que gastou mais dinheiro durante o ano.\n\n");
-	printf("\n********************************************************************************************************************************* \n", KCYN);
+int main(){
+	printf("%s\n************************************************** Sistema de Gestão de Vendas ************************************************** %s\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 1:  Ler ficheiros (os ficheiros disponiveis para leitura são: \'Produtos.txt\', \'Clientes.txt\', \'Vendas_1M.txt\').\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 2:  Determinar a lista de produtos começados por uma Letra à escolha (A...Z).\n" , KCYN, RESET);
+	printf("%s\n\t->%s Query 3:  Dado um mês e um produto, determinar e apresentar o nº total de registos\n\t   de venda e o total facturado com esse produto nesse mês, tendo em conta a distinção N/P,\n\t   há possibilidade de escolher a apresentação dos resultados dividida por filial.\n" , KCYN, RESET);
+	printf("%s\n\t->%s Query 4:  Determinar a lista ordenada de produtos que ninguém comprou, e o seu total,\n\t   há possibilidade de escolher a apresentação dos resultados dividida por filial.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 5:  Determinar a lista ordenada de clientes que realizaram compras em todas as filiais.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 6:  Determinar o nº de clientes registados que não realizaram compras.\n\t   Determinar o nº de produtos que ninguém comprou.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 7:  Dado um cliente, criar uma tabela com o nº total de produtos comprados, mês a mês, organizado por filial.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 8:  Dado um intervalo de tempo (de mês __ a mês __):\n\t   Determinar o total de vendas registadas nesse intervalo e o total facturado.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 9:  Dado um produto e uma filial, determinar os clientes e o nº de clientes que o compraram,\n\t   distinguindo a compra entre N e P.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 10: Dado um cliente e um mês, determinar a lista de\n\t   produtos que esse cliente mais comprou, por ordem decrescente.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query 11: Criar uma lista dos N produtos mais vendidos em todo o ano,\n\t   indicando o nº total de clientes e o nº total de unidades vendidas, filial a filial.\n", KCYN, RESET);
+	printf("%s\n\t->%s Query12: Dado um cliente, determinar os 3 produtos em que gastou mais dinheiro durante o ano.\n\n", KCYN, RESET);
+	printf("%s********************************************************************************************************************************* %s\n", KCYN, RESET);
+	return 0;
 }
 
 int escolhe_Query(){
@@ -89,7 +90,7 @@ int escolhe_Query(){
 int escolhe_file(){
 	int r;
 
-	printf("\n\tQue ficheiro pretende ler? Clientes.txt [1], Produtos.txt [2], Vendas_1M.txt [3], todos [4]\n\t->  ");
+	printf("\n\tQue ficheiro pretende ler? Clientes.txt [1], Produtos.txt [2], Vendas_1M.txt [3], Todos [4]\n\t->  ");
 	scanf("%d", &r);
 	
 	if(r > 4 || r < 1) printf("\n\t O programa falhou na leitura de um número. [1...3]\n");
@@ -104,28 +105,28 @@ void query_1(int num[6]){
 
 	if(r == 1){
 		init_Clientes(num);
-		printf("\n\tFicheiro lido: Clientes.txt\n\tClientes lidos__%d\n\tClientes escritos__%d\n", num[0],num[1]);
+		printf("%s\n\tFicheiro lido: %sClientes.txt\n\t%sClientes lidos__%s%d\n\t%sClientes escritos__%s%d\n",KBLU,RESET,KBLU,RESET,num[0],KBLU,RESET,num[1]);
 	}
 	
 	if(r == 2){
 		init_Produtos(num);
-		printf("\n\tFicheiro lido: Produtos.txt\n\tProdutos lidos__%d\n\tProdutos escritos__%d\n", num[2],num[3]);
+		printf("%s\n\tFicheiro lido: %sProdutos.txt\n\t%sProdutos lidos__%s%d\n\t%sProdutos escritos__%s%d\n",KBLU,RESET,KBLU,RESET,num[2],KBLU,RESET,num[3]);
 	}
 
 	if (r == 3){
 		Clientes clientes = init_Clientes(num);
 		Produtos produtos = init_Produtos(num);
 		init_Vendas(num,produtos,clientes);
-		printf("\n\tFicheiro lido: Vendas_1M.txt\n\tVendas lidas__%d\n\tVendas escritas__%d\n", num[4],num[5]);
+		printf("%s\n\tFicheiro lido: %sVendas_1M.txt\n\t%sVendas lidas__%s%d\n\t%sVendas escritas__%s%d\n",KBLU,RESET,KBLU,RESET num[4],KBLU,RESET,num[5]);
 	}
 
 	if(r == 4){
 		tClientes = init_Clientes(num);
 		tProdutos = init_Produtos(num);
 		init_Vendas(num,tProdutos,tClientes);
-		printf("\n\tFicheiro lido: Clientes.txt\n\tClientes lidos__%d\n\tClientes escritos__%d\n", num[0],num[1]);
-		printf("\n\tFicheiro lido: Produtos.txt\n\tProdutos lidos__%d\n\tProdutos escritos__%d\n", num[2],num[3]);
-		printf("\n\tFicheiro lido: Vendas_1M.txt\n\tVendas lidas__%d\n\tVendas escritas__%d\n", num[4],num[5]);
+		printf("%s\n\tFicheiro lido: %sClientes.txt\n\t%sClientes lidos__%s%d\n\t%sClientes escritos__%s%d\n",KBLU,RESET,KBLU,RESET,num[0],KBLU,RESET,num[1]);
+		printf("%s\n\tFicheiro lido: %sProdutos.txt\n\t%sProdutos lidos__%s%d\n\t%sProdutos escritos__%s%d\n",KBLU,RESET,KBLU,RESET,num[2],KBLU,RESET,num[3]);
+		printf("%s\n\tFicheiro lido: %sVendas_1M.txt\n\t%sVendas lidas__%s%d\n\t%sVendas escritas__%s%d\n",KBLU,RESET,KBLU,RESET num[4],KBLU,RESET,num[5]);
 	}
 }
 
@@ -135,7 +136,7 @@ void query_2(int num[6]){
 	Produtos p = init_Produtos(num);
 	Strings array_imprimir;
 
-	printf("Qual a letra inicial (maiúscula) do código que pretende procurar? [A..Z]\n");
+	printf("%sQual a letra inicial (maiúscula) do código que pretende procurar? [A..Z]%s\n", KMAG, RESET);
 	scanf("%s", letra);
 
 	array_imprimir = meteletra(Produtos p, char letra[0]);
@@ -146,8 +147,8 @@ int navPag(Strings s)
 {
 	char* tecla;
 	int p;
-	printf("Que página pretende imprimir?\n");
-	printf("\t Caso pretenda sair use a tecla espaço\n", );
+	printf("%sQue página pretende imprimir?%s\n", KCYN, RESET);
+	printf("\t %sCaso pretenda sair use a tecla espaço%s\n",KBLU,RESET);
 	scanf("%s", tecla);
 	while(tecla!=32)
 	{
@@ -194,7 +195,7 @@ int navSetas(Strings s) //left was 75, right was 77
 void navegador(Strings s)
 {
 	int op;
-	printf("Pretende escolher a página ou navegar com as setas? \n Escolher página: [1] Navegar com as setas: [2]\n");
+	printf("Pretende %sescolher a página%s ou %snavegar com as setas%s? \n Escolher página: %s[1]%s Navegar com as setas: %s[2]%s\n", KBLU,RESET,KCYN,RESET,KBLU,RESET,KCYN,RESET);
 	scanf("%d", &op);
 	while(p!=32) //spacebar to end program
 	{
@@ -203,12 +204,12 @@ void navegador(Strings s)
 	}
 }
 
-/**
+
 void query_3(int mes, char* code, faturacao f)
 {
 	//numero de vendas deste prod
 	int r;
-	printf("Qual o resultado que pretende obter? \n Filial 1: [1] Filial 2: [2] Filial 3: [3] Global:[4] \n");
+	printf("%sQual o resultado que pretende obter?%s \n Filial 1: [1] Filial 2: [2] Filial 3: [3] Global:[4] \n", KMAG, RESET);
 	scanf("%d", &r);
 	switch (r)
 	{
@@ -227,13 +228,13 @@ void query_3(int mes, char* code, faturacao f)
 	printf("Faturação total dos produtos em desconto: \n");
 	
 }
-*/
+
 
 
 void query_4(Faturacao f)
 {
 	int r;
-	printf("Qual o resultado que deseja obter? \n Filial 1: [1] Filial 2: [2] Filial 3: [3] Global:[4] \n");
+	printf("%sQual o resultado que deseja obter?%s \n Filial 1: [1] Filial 2: [2] Filial 3: [3] Global:[4] \n", KBLU,RESET);
 	scanf("%d", &r);
 	switch (r)
 	{
@@ -251,7 +252,7 @@ void query_4(Faturacao f)
 
 } 
 
-/**
+
 
 void query_5(Filial f)
 {
@@ -264,47 +265,46 @@ void query_6(Catalogo_Clientes cl, Catalogo_Produtos cp, Filial f)
 	//lista de produtos que não foram comprados
 }
 
-**/
 
 void query_7(	)
 {
 	char* code;
-	printf("Insira o código do cliente\n");
+	printf("%sInsira o código do cliente: %s\n", KMAG, RESET);
 	scanf("%s", &code);
 	//nUnidadesCompradas(code, Strings m, num[12][3])
 }
 
-/**
+
 
 void query_8(Faturacao f)
 {
 	int i, f;
-	printf("Indique o primeiro número do intervalo a procurar: \n");
+	printf("%sIndique o primeiro número do intervalo que procura: %s\n", KCYN, RESET);
 	scanf("%d", &i);
-	printf("Indique o ultimo número do intervalo a procurar: \n");
+	printf("%sIndique o ultimo número do intervalo que procura: %s\n",KCYN, RESET);
 	scanf("%d", &f);
 }
 
 void query_9()
 {
 	char* code;
-	printf("Indique o código do produto que procura: \n");
+	printf("%sIndique o código do produto que procura: %s\n", KCYN, RESET);
 	scanf("%s", &code);
 	int f;
-	printf("Indique a filial em que pretende procurar: \n");
+	printf("%sIndique a filial em que pretende procurar: %s\n" KMAG, RESET);
 	scanf("%d", &f);
 	//printar os numeros dos clientes que o compraram
-	printf("Númmero total de clientes que compraram o produto com o preço normal: %d \n", );
-	printf("Númmero total de clientes que compraram o produto em promoção: %d \n", );
+	printf("%s\tNúmmero total de clientes que compraram o produto com o preço normal: %s%d \n", KCYN, RESET);
+	printf("%s\tNúmmero total de clientes que compraram o produto em promoção: %s %d \n", KMAG, RESET);
 }
 
 void query_10()
 {
 	char* code;
-	printf("Indique o código do cliente que procura: \n");
+	printf("%sIndique o código do cliente que procura: %s\n", KBLU, RESET);
 	scanf("%s", &code);
 	int m;
-	printf("Indique a o número do mês: \n");
+	printf("%sIndique a o número do mês: %s\n", KMAG, RESET);
 	scanf("%d", &m);
 	//lista de codigos  dos produtos + comprados por quantidade -> ordem descendente
  
@@ -313,38 +313,38 @@ void query_10()
 void query_11()
 {
 	int n;
-	printf("Indique o número de produtos que quer obter: \n");
+	printf("%sIndique o número de produtos que pretende obter: %s\n", KCYN, RESET);
 	scanf("%d", &n);
 	//lista de codigos de prods + comprados
 	//numero de clientes que compraram
-	printf("%d clientes adquiriram os %d produtos\n", ,n);
+	printf("\t%d clientes adquiriram os %d produtos\n", ,n);
 	//numero de units vendidas
-	printf("Foram vendidas %d unidades \n", ,n);
+	printf("\tForam vendidas %d unidades \n", ,n);
 	//filial a filial 
 }
 
 void query_12()
 {
 	char* code;
-	printf("Indique o código do cliente: \n");
+	printf("%sIndique o código do cliente: %s\n", KBLU, RESET);
 	scanf("%s", &code);
 	//codigo dos 3 produtos em que mais gastou
 
 }
 
-*/
+
 
 void toprintornottoprint()
 {
 	char* r = 0;
-	printf("Pretende dar print ao menu? Yes: [y] No: [n]\n");
+	printf("Pretende apresentar o menu? Yes: [y] No: [n]\n");
 	scanf("%c", r);
 
 	if(r[0] == 'y')
 		print_menu();
 
 	if(r[0] == 'n'){
-		printf("Escolha a opção que pretende inicar [1...12]\n");
+		printf("Escolha a opção a inicar [1...12]\n");
 		escolhe_Query();
 	}
 }
@@ -376,3 +376,4 @@ void divisao(int num, int i)
 		
 	}
 }
+
