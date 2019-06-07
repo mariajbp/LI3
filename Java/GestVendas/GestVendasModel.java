494b345e2a84e39727a68bd61c8bdcf5efefbb90
Francisco Peixoto <thtr.irubra@gmail.com>
2019-06-08 00:58:24 +0100

    idk guys

--- Java/GestVendas/GestVendasModel.java
+++ Java/GestVendas/GestVendasModel.java
@@ -320,6 +320,7 @@ public class GestVendasModel implements Serializable, IGestVendasModel
         e.setClientesMF(cMF);
     }
     
+    public Estatisticas getEstatisticas(){return e;}
         
     /**** QUERY1 ****/
     /** 
