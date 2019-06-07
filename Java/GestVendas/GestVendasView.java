46c6358f4fc25b881f01d782fdb6e312ea710aad
Francisco Peixoto <thtr.irubra@gmail.com>
2019-06-07 13:04:53 +0100

    add printStats

--- Java/GestVendas/GestVendasView.java
+++ Java/GestVendas/GestVendasView.java
@@ -278,5 +278,10 @@ public class GestVendasView implements Serializable, IGestVendasView
        
    }
    
+   public void printStats(Estatisticas e)
+   {
+      out.println(e.toString());
+   }
+   
    public void time(double t) {out.println("\nTempo demorado: " + t + "segundos"); }
 }
